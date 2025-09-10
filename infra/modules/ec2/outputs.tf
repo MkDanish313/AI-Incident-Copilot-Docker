21ebdac5c3960277b83a59f081e50e3e6119235d
Dansih <malakdanish772@gmail.com>
2025-09-10 11:53:32 +0530

    new-config

--- infra/modules/ec2/outputs.tf
+++ infra/modules/ec2/outputs.tf
@@ -3,12 +3,12 @@ output "instance_id" {
   value       = aws_instance.this.id
 }
 
-output "instance_public_ip" {
+output "public_ip" {
   description = "Public IP of the EC2 instance"
   value       = aws_instance.this.public_ip
 }
 
-output "instance_public_dns" {
+output "public_dns" {
   description = "Public DNS of the EC2 instance"
   value       = aws_instance.this.public_dns
 }
--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,10 +1,22 @@
-output "public_ip" {
-  value = module.ec2.public_ip
+output "instance_id" {
+  description = "EC2 instance ID from module"
+  value       = module.ec2.instance_id
 }
+
+output "public_ip" {
+  description = "EC2 Public IP from module"
+  value       = module.ec2.public_ip
+}
+
+output "public_dns" {
+  description = "EC2 Public DNS from module"
+  value       = module.ec2.public_dns
+}
+
 output "urls" {
+  description = "Application URLs"
   value = {
     api      = "http://${module.ec2.public_ip}:8000/health"
     frontend = "http://${module.ec2.public_ip}:8501"
-    ollama   = "http://${module.ec2.public_ip}:11434"
   }
 }
